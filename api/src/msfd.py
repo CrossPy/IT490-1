4ef70f3deb79908159549a2c983787dbbef07710
edman80 <eddie.fiorentine@gmail.com>
2017-09-21 10:58:55 -0400

    Changes to msf daemon

--- api/src/msfd.py
+++ api/src/msfd.py
@@ -14,13 +14,16 @@ interval = parse.get('main', 'interval')
 msf = MySportsFeeds(version=api_ver)
 msf.authenticate(api_username,api_password)
 
-def get_latest_data(sport):
+def get_latest_data(sport, msf):
 	#To-Do: Get all games in the next 14 days for given sport	
-	
+	rtdata = msf.msf_get_data(league=sport)
 
 def main():
 	if __name__ == '__main__':
 		#Main service loop
 		while(True):
+			get_latest_data('nfl',msf)
+			get_latest_data('nba',msf)
+			get_latest_data('mlb',msf)
 			time.sleep(interval)
 main()
