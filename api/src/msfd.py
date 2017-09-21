cee39d8ffe7bc777b667507133762594774e88ad
edman80 <eddie.fiorentine@gmail.com>
2017-09-20 23:31:52 -0400

    added api daemon

---
+++ api/src/msfd.py
@@ -0,0 +1,26 @@
+from configparser import SafeConfigParser
+from ohmysportsfeedspy import MySportsFeeds
+
+#Init conf.ini
+parse = SafeConfigParser()
+parse.read('conf.ini')
+
+#Grab consts from conf.ini
+api_ver = parse.get('main','api_ver')
+api_username = parse.get('main', 'api_username')
+api_password = parse.get('main', 'api_password')
+interval = parse.get('main', 'interval')
+#Init MSF Connection
+msf = MySportsFeeds(version=api_ver)
+msf.authenticate(api_username,api_password)
+
+def get_latest_data(sport):
+	#To-Do: Get all games in the next 14 days for given sport	
+	
+
+def main():
+	if __name__ == '__main__':
+		#Main service loop
+		while(True):
+			time.sleep(interval)
+main()
