44f9e2741650c412b27d5f244f7bc73fc8c83ae8
edman80 <eddie.fiorentine@gmail.com>
2017-09-27 20:22:16 -0400

    Added more stuf..

---
+++ web/src/sql_query_mq.php
@@ -0,0 +1,26 @@
+<?php
+	
+	$mqServer = '192.168.1.105';
+	$mQQ = 'sqlQ'
+	$oneTimeKey = uniq()
+	require_once __DIR__ . '/vendor/autoload.php';
+	use PhpAmqpLib\Connection\AMQPStreamConnection;
+	use PhpAmqLib\Message\AMQPMessage;
+	$conn = new AMQPStreamConnection($mqServer, 5672, 'guest', 'guest');
+	$channel = $conn->channel();
+	$msg = new AMQPMessage($oneTimeKey . ' ' . $_POST['query']);
+	$channel->basic_publish(msg, '', $mQQ);
+	
+	$channel->queue_declare($oneTimeKey,false,false,false,false);
+	
+	$callback = function($msg) {
+		echo $msg->body;
+	};
+	$channel->basic_consume($oneTimeKey, '', false, true, false, false, $callback);
+	
+	while(count($channel->callbacks)){
+		$channel->wait();
+	}
+	
+	
+?>
