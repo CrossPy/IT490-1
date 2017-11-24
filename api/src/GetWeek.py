a0cac6b824e143ab67126eff30b4429ccce5f726
Steve St. Edward <32103196+sms46@users.noreply.github.com>
2017-11-24 18:54:40 -0500

    Api update
    Updated API to get event up to a week in advance. Prevoius version would only get current day.

--- api/src/GetWeek.py
+++ api/src/GetWeek.py
@@ -5,7 +5,7 @@ import json
 import pika
 
 #RABBIT_HOST = '192.168.1.110'
-RABBIT_HOST = '10.224.134.149'
+RABBIT_HOST = '192.168.0.110'
 RABBIT_PORT = 5672
 #RABBIT_Q = 'backendQueue'
 RABBIT_Q = '*'
@@ -22,9 +22,9 @@ def getDailyData(sport, username, password, date):
 
 	try:
 		response = requests.get(
-			url="https://api.mysportsfeeds.com/v1.1/pull/"+sport+"/current/daily_game_schedule.json",
+			url="https://api.mysportsfeeds.com/v1.1/pull/"+sport+"/current/full_game_schedule.json",
 			params={
-				"fordate": date
+				"date": "from-today-to-7-days-from-now"
 			},
 			headers={
 				"Authorization":"Basic " + base64.b64encode('{}:{}'.format(username, password).encode('utf-8')).decode('ascii')
@@ -37,24 +37,21 @@ def getDailyData(sport, username, password, date):
 		
 def getWeeklyData(sport, username, password):
 	rtndata = []
-	today = datetime.date.today()
-	numdays = 7
-	date_list = [today - datetime.timedelta(days=x) for x in range(0, numdays)]
-	for day in date_list:
-		rtndata.append(getDailyData(sport, username, password, day))
+	day = datetime.date.today()
+	rtndata = getDailyData(sport, username, password, day)
 	
 	return rtndata
 
 def insertData(weekOfData, sport):
-	for day in weekOfData:
-		print(day)
-		for game in day['dailygameschedule']["gameentry"]:
-			identifier =  game["id"]
-			date = game["date"]
-			time = game["time"]
-			awayTeam = game["awayTeam"]["Name"]
-			homeTeam = game["homeTeam"]["Name"]
-			sendRequest(RABBIT_HOST, RABBIT_Q, RABBIT_USER, RABBIT_PASS, RABBIT_VH, RABBIT_EX, RABBIT_PORT, 'insert_game_data', identifier, date, time, awayTeam, homeTeam, sport)
+	print(weekOfData)
+	for game in weekOfData['fullgameschedule']["gameentry"]:
+		identifier =  game["id"]
+		date = game["date"]
+		time = game["time"]
+		awayTeam = game["awayTeam"]["Name"]
+		homeTeam = game["homeTeam"]["Name"]
+		
+		sendRequest(RABBIT_HOST, RABBIT_Q, RABBIT_USER, RABBIT_PASS, RABBIT_VH, RABBIT_EX, RABBIT_PORT, 'insert_game_data', identifier, date, time, awayTeam, homeTeam, sport)
 
 def sendRequest(rabbitServer, rabbitQ, rabbitUser, rabbitPass, rabbitVHost, rabbitEx, rabbitPort, reqType, identifier, date, time, awayTeam, homeTeam, sport):
 	json_encoded_data = json.dumps({'type':reqType,'identifier':identifier,'date':date,'time':time,'awayTeam':awayTeam,'homeTeam':homeTeam, 'sport':sport})
