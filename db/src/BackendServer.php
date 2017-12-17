f24b928212ca15b7fc2609d3ff832a6aa76fa4f8
steve st edward <sms46@njit.edu>
2017-12-17 15:32:50 -0500

    Backend server updated to process games results

--- db/src/BackendServer.php
+++ db/src/BackendServer.php
@@ -1,18 +1,18 @@
 !/usr/bin/php
 <?php
+	error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+	ini_set('display_errors' , 1);
 	require_once('path.inc');
 	require_once('get_host_info.inc');
 	require_once('rabbitMQLib.inc');
 	$configs = include('server_config.php');
 	print_r($configs);
-	function requestProcessor($request)
-	{
+	function requestProcessor($request){
 		global $response;
 
 		if(!isset($request['type'])){return "ERROR: unsupported message type";}
 
-		switch ($request['type'])
-		{
+		switch ($request['type']){
 			case "login":
 				print_r($request);
 				return doLogin($request['email'],$request['password']);
@@ -56,23 +56,22 @@
 		if ($count == 1){
 			if (password_verify($password, $pass)){
 				$response = "0";
-                                $sqlLog = "insert into event_log values (NOW(), 'Response Code 0: Email $email sucessfully logged in.')";
-                                $logging = $result=mysqli_query($con, $sqlLog);
+				$sqlLog = "insert into event_log values (NOW(), 'Response Code 0: Email $email sucessfully logged in.')";
+				$logging = $result=mysqli_query($con, $sqlLog);
 
 				return $response;
 			}
 		}else{
 			$response = "1";
 			$sqlLog = "insert into event_log values (NOW(), 'Response Code 1: Email $email Failed login attempt')";
-                        $logging = $result=mysqli_query($con, $sqlLog);
+            $logging = $result=mysqli_query($con, $sqlLog);
 
 			return $response;
 		}
 
 	}
 
-	function doRegister($email, $password, $firstName, $lastName)
-	{
+	function doRegister($email, $password, $firstName, $lastName) {
 		$register = fopen("register.txt", "a") ;
 		$date = date("Y-m-d");
 		$time = date("h:i:sa");
@@ -85,29 +84,25 @@
 		$count=mysqli_num_rows($result);
 
 
-		if ($count >= 1)
-		{
-				//email already registered
-				$response = "1";
-				$log = "$date $time Response Code 1: Email $email already registered.\n";
+		if ($count >= 1){
+			//email already registered
+			$response = "1";
+			$log = "$date $time Response Code 1: Email $email already registered.\n";
 
-				$sqlLog = "insert into event_log values (NOW(), 'Response Code 1: Email $email already registered.')";
-				$logging = $result=mysqli_query($con, $sqlLog);
-
-				fwrite($register, $log);
-				return $response;	
-		}else{
-
+			$sqlLog = "insert into event_log values (NOW(), 'Response Code 1: Email $email already registered.')";
+			$logging = $result=mysqli_query($con, $sqlLog);
+			return $response;	
+		}
+		else{
 			$sql="INSERT INTO users (email, password, firstName, lastName, balance) VALUES('$email', '$password', '$firstName', '$lastName', 100)";
-			if (mysqli_query ($con,$sql))
-			{
+			if (mysqli_query ($con,$sql)){
 				//echo mysqli_error($con);
 				//inserted into database
 				$response = "$email";
 				$log = "$date $time Response Code 0: Email $email successfully added to database.\n";
 
 				$sqlLog = "insert into event_log values (NOW(), 'Response Code 0: Email $email successfully added to database.')";
-                                $loggin = $result=mysqli_query($con, $sqlLog);
+				$loggin = $result=mysqli_query($con, $sqlLog);
 				
 				fwrite($register, $log);
 				return $response;
@@ -115,8 +110,7 @@
 		}
 	}
 
-	function getProfile($email)
-	{
+	function getProfile($email){
 		global $configs;	
 
 		$con = new mysqli($configs['SQL_Server'],$configs['SQL_User'],$configs['SQL_Pass'],$configs['SQL_db']);
@@ -130,33 +124,54 @@
 		$response['history'] = array();
 		
 		while ($row = $history->fetch_assoc()){
+			print_r($row);
 			array_push($response['history'], $row);
-		}
-		print_r($response['history']);
-		
+		}		
 		return $response;
 	}
 	
 	
 	function insert_game_data($result) {
 		global $configs;
+		$id = $result["identifier"];
+		$time = $result['time'];
+		$date = $result['date'];
+		$sport = $result['sport'];
+		$homeTeam = $result['homeTeam'];
+		$awayTeam = $result['awayTeam'];
+		$homeScore = $result['homeScore'];
+		$awayScore = $result['awayScore'];
+		
 		$con = new mysqli($configs['SQL_Server'],$configs['SQL_User'],$configs['SQL_Pass'],$configs['SQL_db']);
 
-		$sql="select id from games where id='" . $result["identifier"] ."'";
-
+		$sql="select id, win from games where id='$id'";
 		$query = $con->query($sql);
 		$count = mysqli_num_rows($query);
-		
 		if ($count < 1) {
-			$date = date("Y-m-d H:i:s", strtotime($result['time'] . " " . $result['date']));
-			$sql = "INSERT INTO games (id, sport, team1, team2, start) 
-				VALUES(" .$result['identifier'] . ", '" . $result['sport'] . "', '" . $result['homeTeam'] . "', '" . $result['awayTeam'] . "', '$date')";
+			$datetime = date("Y-m-d H:i:s", strtotime("$time $date"));
+			$sql = "INSERT INTO games (id, sport, team1, team2, start) VALUES($id, '$sport', '$homeTeam', '$awayTeam', '$datetime')";
 			$insert = $con->query($sql);
-			echo $con->error;
+			if (!$insert->error) {
+			   printf("Error message: %s\n", $con->error);
+			}
 			
-			$sqlLog = "insert into event_log values (NOW(), 'Game ID " . $result['identifier'] . " inserted into database')";
-                        $loggin = $con->query($sqlLog);
-			echo $con->error;
+			$sqlLog = "INSERT INTO event_log VALUES (NOW(), 'Game ID $id inserted into database')";
+			$loggin = $con->query($sqlLog);
+			if (!$loggin->error) {
+			   printf("Error message: %s\n", $con->error);
+			}
+		}
+		elseif(empty(mysqli_fetch_assoc($query)['win']) && $homeScore != $awayScore) {
+			if ($homeScore > $awayScore) {
+				$winner = $homeTeam;
+			}
+			else {
+				$winner = $awayTeam;
+			}
+			$update = $con->query("UPDATE games SET score1 = $homeScore, score2 = $awayScore, win = '$winner' where id='$id'");
+			if (!$update->error) {
+			   printf("Error message: %s\n", $con->error);
+			}
 		}
 		$con->close();
 	}
@@ -185,26 +200,25 @@
 					break;
 				}
 				case 'mlb': {
-                                        array_push($mlb, array("id"=>$row['id'], "team1"=>$row['team1'], "team2"=>$row['team2'], "start"=>$row['start']));
+					array_push($mlb, array("id"=>$row['id'], "team1"=>$row['team1'], "team2"=>$row['team2'], "start"=>$row['start']));
 					break;
-                                }
+				}
 				case 'nfl': {
-                                        array_push($nfl, array("id"=>$row['id'], "team1"=>$row['team1'], "team2"=>$row['team2'], "start"=>$row['start']));
+					array_push($nfl, array("id"=>$row['id'], "team1"=>$row['team1'], "team2"=>$row['team2'], "start"=>$row['start']));
 					break;
-                                }
+				}
 			}
 		}		
 		$return = array('nba'=>$nba, 'nfl'=>$nfl, 'mlb'=>$mlb);
-		//print_r($return);
 		$response = json_encode($return);
 
 		return $response;
 	}
 
 	 function bet($result) {
-                global $configs;
+		global $configs;
 
-                $con = new mysqli($configs['SQL_Server'], $configs['SQL_User'], $configs['SQL_Pass'], $configs['SQL_db']);
+		$con = new mysqli($configs['SQL_Server'], $configs['SQL_User'], $configs['SQL_Pass'], $configs['SQL_db']);
 
 		$check = $con->query("select balance from users where email = '" . $result['email'] . "'");
 		$row = $check->fetch_assoc();
@@ -234,8 +248,8 @@
 			$con->query("update users set balance = balance - " . $result['amount'] . " where email = '" . $result['email'] . "'");
 
 			$sqlLog = "insert into event_log values (NOW(), 'user " . $result['email'] . " placed " . $result['amount'] . " bet on " 
-			. $result['team'] . " for game id " . $result['id'] . "')";
-                        $loggin = $con->query($sqlLog);
+				. $result['team'] . " for game id " . $result['id'] . "')";
+            $loggin = $con->query($sqlLog);
 
 			printf($con->error);
 			return 1; //succesfully made a bet and current balance updated
