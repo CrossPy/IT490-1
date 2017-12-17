c29fde45706293f05646c8ae498d78d88817a2cf
Steve St. Edward <32103196+sms46@users.noreply.github.com>
2017-12-16 23:27:23 -0500

    Updated api
    updated api to fix errors

--- api/src/GetWeek.py
+++ api/src/GetWeek.py
@@ -1,11 +1,11 @@
 import base64
 import requests
-import datetime
+import datetime 
+from datetime import timedelta
 import json
 import pika
 
-#RABBIT_HOST = '192.168.1.110'
-RABBIT_HOST = '192.168.0.110'
+RABBIT_HOST = '192.168.1.110'
 RABBIT_PORT = 5672
 #RABBIT_Q = 'backendQueue'
 RABBIT_Q = '*'
@@ -19,7 +19,6 @@ API_PASS = 'IT490sucks'
 
 def getDailyData(sport, username, password, date):
     date = date.isoformat().replace("-","")
-    
     try:
         response = requests.get(
             url="https://api.mysportsfeeds.com/v1.1/pull/"+sport+"/current/full_game_schedule.json",
@@ -37,21 +36,21 @@ def getDailyData(sport, username, password, date):
 
         
 def getDailyScores(sport, username, password):
-    day = datetime.date.today()
+    day = datetime.date.today() - timedelta(1)
     date = day.isoformat().replace("-","")
-    print(date)
     try:
         
         response = requests.get(
             url="https://api.mysportsfeeds.com/v1.1/pull/"+sport+"/current/scoreboard.json",
             params={
-                "fordate": date
+                "fordate": date,
+                "status": "final"
             },
             headers={
                 "Authorization":"Basic " + base64.b64encode('{}:{}'.format(username, password).encode('utf-8')).decode('ascii')
             }
         )
-        print(response.text)
+        print(response.text)   
         return json.loads(response.text)
     except requests.exceptions.RequestException:
         print('Request Failed.')
@@ -66,17 +65,18 @@ def getWeeklyData(sport, username, password):
     
 def insertScores(sport):
     scores = getDailyScores(sport, API_USER, API_PASS)
+    games = scores["scoreboard"]["gameScore"]
     
-    for game in scores["scoreboard"]["gameScore"]:
+    for game in games:
         identifier = game["game"]["ID"]
-        homeTeam = game["game"]["homeTeam"]["name"]
-        awayTeam = game["game"]["awayTeam"]["name"]
+        homeTeam = game["game"]["homeTeam"]["Name"]
+        awayTeam = game["game"]["awayTeam"]["Name"]
         date = game["game"]["date"]
         time = game["game"]["time"]
         homeScore = game["homeScore"]
         awayScore = game["awayScore"]
-        
         sendRequest(RABBIT_HOST, RABBIT_Q, RABBIT_USER, RABBIT_PASS, RABBIT_VH, RABBIT_EX, RABBIT_PORT, 'insert_game_data', identifier, date, time, awayTeam, homeTeam, sport, homeScore, awayScore)
+        
 def insertData(weekOfData, sport):
     print(weekOfData)
     for game in weekOfData['fullgameschedule']["gameentry"]:
@@ -87,39 +87,41 @@ def insertData(weekOfData, sport):
         awayTeam = game["awayTeam"]["Name"]
         homeTeam = game["homeTeam"]["Name"]
         
-        sendRequest(RABBIT_HOST, RABBIT_Q, RABBIT_USER, RABBIT_PASS, RABBIT_VH, RABBIT_EX, RABBIT_PORT, 'insert_game_data', identifier, date, time, awayTeam, homeTeam, sport)
+        sendRequest(RABBIT_HOST, RABBIT_Q, RABBIT_USER, RABBIT_PASS, RABBIT_VH, RABBIT_EX, RABBIT_PORT, 'insert_game_data', identifier, date, time, awayTeam, homeTeam, sport, 0, 0)
 
-def sendRequest(rabbitServer, rabbitQ, rabbitUser, rabbitPass, rabbitVHost, rabbitEx, rabbitPort, reqType, identifier, date, time, awayTeam, homeTeam, sport, homeScore=0, awayScore=0):
-    json_encoded_data = json.dumps({'type':reqType,
-                                    'identifier':identifier,
-                                    'date':date,
-                                    'time':time,
-                                    'awayTeam':awayTeam,
-                                    'homeTeam':homeTeam,
-                                    'sport':sport,
-                                    'homeScore':homeScore,
-                                    'awayScore':awayScore
-                                    })
-                                    
+def sendRequest(rabbitServer, rabbitQ, rabbitUser, rabbitPass, rabbitVHost, rabbitEx, rabbitPort, reqType, identifier, date, time, awayTeam, homeTeam, sport, homeScore, awayScore):
+    json_encoded_data = json.dumps({'type': reqType,
+                                    'identifier': identifier,
+                                    'date': date,
+                                    'time': time,
+                                    'awayTeam': awayTeam,
+                                    'homeTeam': homeTeam,
+                                    'sport': sport,
+                                    'homeScore': homeScore,
+                                    'awayScore': awayScore
+                                    })      
     creds = pika.PlainCredentials(rabbitUser, rabbitPass)
     connection = pika.BlockingConnection(pika.ConnectionParameters(rabbitServer, rabbitPort, rabbitVHost, creds))
     channel = connection.channel()
     channel.basic_publish(exchange=rabbitEx, routing_key=rabbitQ, body=json_encoded_data)
+    
 def main():
-    try:
+    try:        
+        #insertData(getWeeklyData('nfl', API_USER, API_PASS), 'nfl')
         insertScores('nfl')
-        insertData(getWeeklyData('nfl', API_USER, API_PASS), 'nfl')
+        print("\nNFL Done")
     except Exception as e:
         print(e)
-    try:
+    try:        
+        #insertData(getWeeklyData('nba', API_USER, API_PASS), 'nba')
         insertScores('nba')
-        insertData(getWeeklyData('nba', API_USER, API_PASS), 'nba')
-        print("NBA Done\n")
+        print("\nNBA Done")
     except Exception as e:
         print(e)
-    try:
+    try:        
+        #insertData(getWeeklyData('mlb', API_USER, API_PASS), 'mlb')
         insertScores('mlb')
-        insertData(getWeeklyData('mlb', API_USER, API_PASS), 'mlb')
+        print("\nMLB Done")
     except Exception as e:
         print("Error!")
         print(e)
