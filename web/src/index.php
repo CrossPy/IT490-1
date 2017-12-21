d3035ec33d2f2a7bfa8585b6fc7fdfb1b23a0a95
edman80 <eddie.fiorentine@gmail.com>
2017-12-20 19:31:39 -0500

    Update index.php
    sup

--- web/src/index.php
+++ web/src/index.php
@@ -19,7 +19,7 @@
 <head>
 	<meta charset="utf-8">		
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>NJIT Bookies | Home</title>
+	<title>NJIT Bookies | Home IT490 Sucks</title>
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>	
